e624c7d197ed483f018a340e0be49afa463d1841
2333 whip1ash <usergeng@gmail.com>
2020-03-10 10:34:01 +0800

    fix a bug

---
+++ src/bin/import_wiki_data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+@author: whip1ash
+@contact: security@whip1ash.cn
+@software: pycharm 
+@file: import_wiki_data.py
+@time: 2020/3/10 09:23
+@desc:
+'''
