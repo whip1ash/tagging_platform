df9577a04300004f9c421d332f489e81cebccac8
2333 whip1ash <usergeng@gmail.com>
2020-03-11 10:16:21 +0800

    add features todo list

--- src/app/normal/views.py
+++ src/app/normal/views.py
@@ -6,6 +6,11 @@ from django.http import JsonResponse
 from app.utils.tools import *
 
 def index(request):
+    '''
+    index 页面两个跳转，一个跳到实体打标，另一个跳到关系打标
+    :param request:
+    :return:
+    '''
     return render(request,'normal/index.html')
 
 def page_not_found(request,exception):
@@ -13,3 +18,62 @@ def page_not_found(request,exception):
 
 def server_error(request):
     return render(request,'normal/500.html')
+
+# todo: url这里添加新的路由:sentence
+def sentence_index(request):
+    '''
+    todo: 查看数据库中现存的句子。页面中发json，取历史记录到前端。
+    :param request:
+    :return:
+    '''
+    # todo: 新建html模板
+    return render(request,'sentence/index.html')
+
+
+def sentence_list(request):
+    '''
+    todo: 返回sentence表中的所有数据，记得分页
+    :param request:
+    :return:
+    '''
+    pass
+
+def sentence_done(request):
+    '''
+    todo: 查看已打标的句子数据，判断来源，是entity、relation、all
+    :param request:
+    :return:
+    '''
+
+    pass
+
+def sentence_doing(request):
+    '''
+    todo: 查看正在打标中的数据，判断来源，entity、relation、all
+    :param request:
+    :return:
+    '''
+
+    pass
+
+def sentence_count(request):
+    '''
+    todo: 返回句子的数量，同样判断三种来源情况。
+    :param request:
+    :return:
+    '''
+    pass
+
+def sentence_get(request):
+    '''
+    todo: 取一条未打标的数据，同样判断三种来源情况
+    :param request:
+    :return:
+    '''
+
+def export(request):
+    '''
+    todo: 数据导出功能，模块化拆出来。通过参数判断导出实体的数据还是关系的数据
+    :param request:
+    :return:
+    '''
--- src/app/relation/views.py
+++ src/app/relation/views.py
@@ -6,6 +6,91 @@ from django.http import JsonResponse
 from app.utils.tools import *
 from .models import RelationTag,RelationType
 
+def index(request):
+    '''
+    todo: 关系打标index页面，存在以下几个链接，查看未/已打标的句子、查看已打标的关系数据、开始打标
+    :param request:
+    :return:
+    '''
+
+    return render(request, 'relation/index.html')
+
+def tag_view(request):
+    '''
+    todo: 打标的页面
+    api: 增加打标/考虑update情况(保存功能)，考虑将add和update两个接口合并、下一条数据(sentence中实现)
+    :param request:
+    :return:
+    '''
+    return render(request, 'relation/tag.html')
+
+
+def tag_history(request):
+    '''
+    todo 历史打标数据的页面
+    api: list、delete、edit、get
+    :param request:
+    :return:
+    '''
+    return render(request, 'relation/history.html')
+
+
+# todo: 以下是api接口
+def save(request):
+    '''
+    todo: save tag
+    :param request:
+    :return:
+    '''
+    pass
+
+
+def list(request):
+    '''
+    todo: 查看历史打标数据,分页
+    :param request:
+    :return:
+    '''
+    pass
+
+
+def count(request):
+    '''
+    todo: 数据数量，考虑是否要合并到list接口中
+    :param request:
+    :return:
+    '''
+    pass
+
+
+def delete(request):
+    '''
+    todo: delete a tag
+    :param request:
+    :return:
+    '''
+    pass
+
+
+def edit(request):
+    '''
+    todo: 此接口保留不实现，edit功能同save
+    :param request:
+    :return:
+    '''
+
+    pass
+
+
+def get(request):
+    '''
+    todo: 拿一条特定的数据。
+    :param request:
+    :return:
+    '''
+    pass
+
+
 def list_relation_type(request):
     response_data = list(RelationType.object.all().value())
     return JsonResponse(success_resp(data=response_data))
