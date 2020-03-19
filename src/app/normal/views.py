4a3a1fb27913412a7b5866dc7273e38d6b95a441
BlackCheck <blackcheck@foxmail.com>
2020-03-19 13:13:41 +0800

    update a part of export functions, but no all of them

--- src/app/normal/views.py
+++ src/app/normal/views.py
@@ -5,9 +5,13 @@ from django.http import JsonResponse
 
 from app.utils.tools import *
 from .models import Sentence
+from ..entity.models import EntityTag,EntityType
+from ..relation.models import RelationTag,RelationType
 from django.forms.models import model_to_dict
 
 import json
+import nltk
+import re
 
 def index(request):
     '''
@@ -15,13 +19,16 @@ def index(request):
     :param request:
     :return:
     '''
-    return render(request,'normal/index.html')
+    return render(request, 'normal/index.html')
 
-def page_not_found(request,exception):
-    return render(request,'normal/404.html')
+
+def page_not_found(request, exception):
+    return render(request, 'normal/404.html')
+
 
 def server_error(request):
-    return render(request,'normal/500.html')
+    return render(request, 'normal/500.html')
+
 
 def sentence_index(request):
     '''
@@ -30,7 +37,7 @@ def sentence_index(request):
     :return:
     '''
     # todo: 新建html模板
-    return render(request,'sentence/index.html')
+    return render(request, 'sentence/index.html')
 
 
 def sentence_list(request):
@@ -47,14 +54,14 @@ def sentence_list(request):
     page = int(body.get("page")) if int(body.get("page")) > 0 else 0
     limit = int(body.get("limit")) if int(body.get("limit")) > 0 else 10
 
-    offset = page2offset(page,limit)
+    offset = page2offset(page, limit)
 
     try:
-        sentences = list(Sentence.objects.all()[offset:offset+limit].values())
+        sentences = list(Sentence.objects.all()[offset:offset + limit].values())
     except Exception as e:
-        return JsonResponse(fail_resp(code=DATABASE_ERROR,msg="List all sentence failed",data=get_exception(e)))
+        return JsonResponse(fail_resp(code=DATABASE_ERROR, msg="List all sentence failed", data=get_exception(e)))
 
-    return JsonResponse(success_resp(msg="List sentences success!",data=sentences))
+    return JsonResponse(success_resp(msg="List sentences success!", data=sentences))
 
 
 def sentence_done(request):
@@ -76,17 +83,19 @@ def sentence_done(request):
 
     try:
         if referer == "entity":
-            tagged_entities = queryset2list(Sentence.objects.filter(entity_tag=True).all()[offset:offset+limit])
+            tagged_entities = queryset2list(Sentence.objects.filter(entity_tag=True).all()[offset:offset + limit])
         elif referer == "relation":
-            tagged_entities = queryset2list(Sentence.objects.filter(relation_tag=True).all()[offset:offset+limit])
+            tagged_entities = queryset2list(Sentence.objects.filter(relation_tag=True).all()[offset:offset + limit])
         elif referer == "all":
-            tagged_entities = queryset2list(Sentence.objects.filter(entity_tag=True,relation_tag=True).all()[offset:offset+limit])
+            tagged_entities = queryset2list(
+                Sentence.objects.filter(entity_tag=True, relation_tag=True).all()[offset:offset + limit])
         else:
-            return JsonResponse(fail_resp(code=WRONG_PARAM_CODE,msg="Input a invalid referer"))
+            return JsonResponse(fail_resp(code=WRONG_PARAM_CODE, msg="Input a invalid referer"))
     except Exception as e:
-        return JsonResponse(fail_resp(code=DATABASE_ERROR,msg="List done sentences failed!",data=get_exception(e)))
+        return JsonResponse(fail_resp(code=DATABASE_ERROR, msg="List done sentences failed!", data=get_exception(e)))
 
-    return JsonResponse(success_resp(msg="List done sentences success!",data=tagged_entities))
+    return JsonResponse(success_resp(msg="List done sentences success!", data=tagged_entities))
+
 
 def sentence_doing(request):
     '''
@@ -111,9 +120,10 @@ def sentence_doing(request):
         elif referer == "relation":
             tagging_entities = queryset2list(Sentence.objects.filter(relation_tag=False).all()[offset:limit])
         elif referer == "all":
-            tagging_entities = queryset2list(Sentence.objects.exclude(entity_tag=True, relation_tag=True).all()[offset:limit])
+            tagging_entities = queryset2list(
+                Sentence.objects.exclude(entity_tag=True, relation_tag=True).all()[offset:limit])
         else:
-            return JsonResponse(fail_resp(code=WRONG_PARAM_CODE,msg="Input a invalid referer"))
+            return JsonResponse(fail_resp(code=WRONG_PARAM_CODE, msg="Input a invalid referer"))
     except Exception as e:
         return JsonResponse(fail_resp(code=DATABASE_ERROR, msg="List doing sentences failed!", data=get_exception(e)))
 
@@ -142,14 +152,16 @@ def sentence_count(request):
             done_num = Sentence.objects.filter(relation_tag=True).count()
         elif referer == "all":
             all_num = Sentence.objects.all().count()
-            done_num = Sentence.objects.filter(relation_tag=True,entity_tag=True).count()
+            done_num = Sentence.objects.filter(relation_tag=True, entity_tag=True).count()
             doing_num = all_num - done_num
         else:
-            return JsonResponse(fail_resp(code=WRONG_PARAM_CODE,msg="Input a invalid referer"))
+            return JsonResponse(fail_resp(code=WRONG_PARAM_CODE, msg="Input a invalid referer"))
     except Exception as e:
-        return JsonResponse(fail_resp(code=DATABASE_ERROR,msg="Count sencence failed!",data=get_exception(e)))
+        return JsonResponse(fail_resp(code=DATABASE_ERROR, msg="Count sencence failed!", data=get_exception(e)))
 
-    return JsonResponse(success_resp(msg="Get sentence num success!",data={"doing_num":doing_num,"done_num":done_num}))
+    return JsonResponse(
+        success_resp(msg="Get sentence num success!", data={"doing_num": doing_num, "done_num": done_num}))
+
 
 def sentence_get(request):
     '''
@@ -172,17 +184,101 @@ def sentence_get(request):
         else:
             return JsonResponse(fail_resp(code=WRONG_PARAM_CODE, msg="Input a invalid referer"))
     except Exception as e:
-        return JsonResponse(fail_resp(code=DATABASE_ERROR,msg="Get a sentence failed!",data=get_exception(e)))
+        return JsonResponse(fail_resp(code=DATABASE_ERROR, msg="Get a sentence failed!", data=get_exception(e)))
 
-    return JsonResponse(success_resp(msg="Get a sentence success",data=sentence))
+    return JsonResponse(success_resp(msg="Get a sentence success", data=sentence))
 
 
 def export(request):
     '''
     todo: 数据导出功能，模块化拆出来。通过参数判断导出实体的数据还是关系的数据
-    :param request:
-    :return:
+    :param request:{'referer':'entity'}
+    :return: xxx.txt
     '''
+    entity_dict = {}
+    tmp_sentence = queryset2list(Sentence.objects.all().values())
+    for i in tmp_sentence:
+        tmp_sen = Sen(i)
+        if tmp_sen.entity:
+            # storage format:{'id':int, 'word':word_content, 'label':'O'or"entity_type"}
+            # split sentence(nltk.word_tokenize()) -> find sentence_id in tag table -> word[pos] : entity[entity_id]
+            # sentence_id = i.get('id')
+
+            # for j in tmp_spilt_sentence:
+            #     tmp_word_dict = {'id': }
+            #     entity_dict.add
+            pass
+
+
+class Sen:
+    def __init__(self,tmp):
+        '''
+        初始化加载Sentence表中的当前行数据
+        :param tmp: {'id':int,'content':str,'entity':bool,'relation':bool}
+        '''
+        self.id = tmp.get('id')
+        self.content = tmp.get('content')
+        self.entity = tmp.get('entity_tag')
+        self.relation = tmp.get('relation_tag')
+
+    def word_split(self,str):
+        '''
+        分割句子
+        :param str: 'I am a man.'
+        :return: ['I', 'am', 'a', 'man', '.']
+        '''
+        return nltk.word_tokenize(str)
+
+    def find_entity_tag(self,sen_id):
+        '''
+        返回list类型的entity_tag表中符合sen_id的数据
+        :param sen_id: int
+        :return:[{'id':int,'pos':"x,y",'entity':str,'sentence_id':int,'type_id',int},...]
+        '''
+        return queryset2list(EntityTag.objects.filter(sentence_id=sen_id))
+
+    def find_relation_tag(self,sen_id):
+        '''
+        返回list类型的relation_tag表中符合sen_id的数据
+        :param sen_id: int
+        :return:[{
+    		"tag_id":int,
+    		"sentence_id":int,
+    		"head_entity_pos":"x(int),y(int)",
+    		"head_entity":string,
+    		"tail_entity_pos":"x(int),y(int)",
+    		"tail_entity":string,
+    		"type_id":int
+		},...]
+        '''
+        return queryset2list(RelationTag.objects.filter(sentence_id=sen_id))
+
+    def output_entity_training_data(self,sen,sen_id):
+        splited_sen = self.word_split(sen)
+        res = []
+        count = 0
+        tag_data = self.find_entity_tag(sen_id)
+        entity_type = self.generate_printed_entity_type(tag_data,len(splited_sen))
+        for i in splited_sen:
+            count += 1
+            tmp = {'id':self.id,'word':i,'type':entity_type[count-1]}
+            res.append(tmp)
+        return res
+
+    def generate_printed_entity_type(self,tag_data,l):
+        '''
+        通过对生成第三列entity_type需要的数据
+        :param tag_data:
+        :param l:
+        :return:
+        '''
+        res = ['O']*l
+        for i in tag_data:
+            tmp_pos = re.split(r'\W+',i.get('pos'))
+            entity_id = i.get('type_id')
+            res[tmp_pos[0]:tmp_pos[1]] = queryset2list(EntityTag.objects.get(pk=entity_id))[0].get('name')*(tmp_pos[1]-tmp_pos[0])
+        return res
+
 
 
 def queryset2list(queryset):
