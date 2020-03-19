eed4c72a6d9dc5150a1077c5a5de12cf457b16dc
BlackCheck <blackcheck@foxmail.com>
2020-03-19 16:40:35 +0800

    finish export function

--- src/app/normal/views.py
+++ src/app/normal/views.py
@@ -5,13 +5,14 @@ from django.http import JsonResponse
 
 from app.utils.tools import *
 from .models import Sentence
-from ..entity.models import EntityTag,EntityType
-from ..relation.models import RelationTag,RelationType
+from ..entity.models import EntityTag, EntityType
+from ..relation.models import RelationTag, RelationType
 from django.forms.models import model_to_dict
 
 import json
 import nltk
 import re
+
 
 def index(request):
     '''
@@ -195,23 +196,25 @@ def export(request):
     :param request:{'referer':'entity'}
     :return: xxx.txt
     '''
-    entity_dict = {}
+    data = []
+    body = json.loads(request.body)
+    referer = body.get("referer")
     tmp_sentence = queryset2list(Sentence.objects.all().values())
     for i in tmp_sentence:
         tmp_sen = Sen(i)
-        if tmp_sen.entity:
+        if tmp_sen.entity & referer == "entity":
             # storage format:{'id':int, 'word':word_content, 'label':'O'or"entity_type"}
             # split sentence(nltk.word_tokenize()) -> find sentence_id in tag table -> word[pos] : entity[entity_id]
-            # sentence_id = i.get('id')
-
-            # for j in tmp_spilt_sentence:
-            #     tmp_word_dict = {'id': }
-            #     entity_dict.add
+            data.append(tmp_sen.output_entity_training_data(tmp_sen.content,tmp_sen.id))
+        elif tmp_sen.relation & referer == "relation":
+            data.append(tmp_sen.output_relation_training_data(tmp_sen.content,tmp_sen.id))
+        else:
             pass
+    tmp_sen.print_into_file(referer,data)
 
 
 class Sen:
-    def __init__(self,tmp):
+    def __init__(self, tmp):
         '''
         初始化加载Sentence表中的当前行数据
         :param tmp: {'id':int,'content':str,'entity':bool,'relation':bool}
@@ -221,23 +224,27 @@ class Sen:
         self.entity = tmp.get('entity_tag')
         self.relation = tmp.get('relation_tag')
 
-    def word_split(self,str):
+    def word_split(self, content):
         '''
         分割句子
-        :param str: 'I am a man.'
+        :param content: 'I am a man.'
         :return: ['I', 'am', 'a', 'man', '.']
         '''
-        return nltk.word_tokenize(str)
+        return nltk.word_tokenize(content)
 
-    def find_entity_tag(self,sen_id):
+    def find_entity_tag(self, sen_id):
         '''
         返回list类型的entity_tag表中符合sen_id的数据
         :param sen_id: int
         :return:[{'id':int,'pos':"x,y",'entity':str,'sentence_id':int,'type_id',int},...]
         '''
-        return queryset2list(EntityTag.objects.filter(sentence_id=sen_id))
+        try:
+            if EntityTag.objects.filter(sentence_id=sen_id) != None:
+                return queryset2list(EntityTag.objects.filter(sentence_id=sen_id))
+        except e:
+            print("No tagged entity in this Sentence")
 
-    def find_relation_tag(self,sen_id):
+    def find_relation_tag(self, sen_id):
         '''
         返回list类型的relation_tag表中符合sen_id的数据
         :param sen_id: int
@@ -251,34 +258,101 @@ class Sen:
     		"type_id":int
 		},...]
         '''
-        return queryset2list(RelationTag.objects.filter(sentence_id=sen_id))
+        try:
+            if EntityTag.objects.filter(sentence_id=sen_id) != None:
+                return queryset2list(RelationTag.objects.filter(sentence_id=sen_id))
+        except e:
+            print("No tagged relation in this Sentence")
 
-    def output_entity_training_data(self,sen,sen_id):
+    def output_entity_training_data(self, sen, sen_id):
+        '''
+        生成输出需要的三列各自的数据
+        :param sen: str(句子)
+        :param sen_id: sentence_id
+        :return: [{'id': int, 'word': str, 'type': str},...]
+        '''
         splited_sen = self.word_split(sen)
         res = []
         count = 0
         tag_data = self.find_entity_tag(sen_id)
-        entity_type = self.generate_printed_entity_type(tag_data,len(splited_sen))
+        entity_type = self.generate_printed_entity_type(tag_data, len(splited_sen))
         for i in splited_sen:
             count += 1
-            tmp = {'id':self.id,'word':i,'type':entity_type[count-1]}
+            tmp = {'id': self.id, 'word': i, 'type': entity_type[count - 1]}
             res.append(tmp)
         return res
 
-    def generate_printed_entity_type(self,tag_data,l):
+    def generate_printed_entity_type(self, tag_data, l):
         '''
         通过对生成第三列entity_type需要的数据
-        :param tag_data:
-        :param l:
+        :param tag_data: 符合sentence_id的打标数据
+        :param l: 句子包含的词及标点的个数
         :return:
         '''
-        res = ['O']*l
+        res = ['O'] * l
         for i in tag_data:
-            tmp_pos = re.split(r'\W+',i.get('pos'))
+            tmp_pos = self.get_pos(i.get('pos'))
             entity_id = i.get('type_id')
-            res[tmp_pos[0]:tmp_pos[1]] = queryset2list(EntityTag.objects.get(pk=entity_id))[0].get('name')*(tmp_pos[1]-tmp_pos[0])
+            res[tmp_pos[0]:tmp_pos[1]] = queryset2list(EntityTag.objects.get(pk=entity_id))[0].get('name') * (
+                    tmp_pos[1] - tmp_pos[0] + 1)
         return res
 
+    def output_relation_training_data(self, sen, sen_id):
+        '''
+
+        :param sen:
+        :param sen_id:
+        :return:
+        '''
+        splited_sen = self.word_split(sen)
+        res = []
+        count = 0
+        tag_data = self.find_relation_tag(sen_id)
+        relation_type = self.generate_printed_relation_type(tag_data, len(splited_sen))
+        for i in splited_sen:
+            count += 1
+            # tmp = {'id': self.id, 'word': i, 'type': entity_type[count - 1]}
+            tmp = self.generate_printed_relation_type(tag_data,splited_sen)
+            res.append(tmp)
+        return res
+
+    def generate_printed_relation_type(self, tag_data, l):
+        '''
+        通过对生成第三列entity_type需要的数据
+        :param tag_data: 符合sentence_id的打标数据
+        :param l: 句子包含的词及标点的个数
+        :return:
+        '''
+        token = splited_sen
+        h = {"name":tag_data[0].get('head_entity'),"pos":self.get_pos(tag_data[0].get('head_entity_pos'))}
+        t = {"name":tag_data[0].get('tail_entity'),"pos":self.get_pos(tag_data[0].get('tail_entity_pos'))}
+        relation = queryset2list(RelationType.objects.get(pk=tag_data[0].get('type_id')))[0].get('name')
+        res = {"token":token, "h":h, "t":t, "relation":relation}
+        return res
+
+    @staticmethod
+    def get_pos(self,str):
+        tmp = re.split(r'\W+',str)
+        res = []
+        for i in tmp:
+            res.append(int(i))
+        return res
+
+    def print_into_file(self,referer,data):
+        if referer == "entity":
+            fo = open("train_entity.txt","a+")
+            entity_data = data
+            for i in entity_data:
+                print(i.get('id'),' ',i.get('word'),' ',i.get('type'),'\n')
+            fo.close()
+        elif referer == "relation":
+            fo = open("train_entity.json","a+")
+            relation_data = data
+            for i in relation_data:
+                print(i,'\n')
+            fo.close()
+        else:
+            print("param error")
 
 
 def queryset2list(queryset):
